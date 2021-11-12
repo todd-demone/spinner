336a7db6b93a1d19a4c08ffe058435e906bca26a
Todd Demone <tdemone1@gmail.com>
2021-11-12 17:01:36 +0000

    complete Spinner II assignment

--- spinner2.js
+++ spinner2.js
@@ -1,7 +1,7 @@
 const spinnerChars = ['|', '/', '-', '\\', '|'];
 let time = 100;
 for (let i = 0; i < spinnerChars.length; i++) {
-  setTimeout(() => {
+  setTimeout((time) => {
     process.stdout.write(`\r${spinnerChars[i]}   `);
     if (i === spinnerChars.length - 1) process.stdout.write('\n');
   }, time);
